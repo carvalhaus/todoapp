02bf4baafc3f2eedf1af8ea30293def81573b1dc
carvalhaus <ooliveira.joaop@gmail.com>
2023-11-19 17:18:26 -0300

    feat: add Header into ToDoApp

---
+++ client/src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import Button from "../button";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Get Things Done</h1>
+      <div className="button_container">
+        <Button text="Add new" className="add-new" />
+        <Button text="Sign Out" className="sign-out" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
