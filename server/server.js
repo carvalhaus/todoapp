b30c725fda95b859893f9fc961d1a0722255432e
carvalhaus <ooliveira.joaop@gmail.com>
2023-11-20 07:24:14 -0300

    feat: create db and server files, add environment variables

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const PORT = process.env.PORT ?? 8000;
+const express = require("express");
+const app = express();
+const pool = require("./db");
+
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await pool.query("SELECT * FROM todos");
+    res.json(todos.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
