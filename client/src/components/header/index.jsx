5fd95c4a44d56885cdb59db1f4633342d04f6049
carvalhaus <ooliveira.joaop@gmail.com>
2023-11-24 16:00:55 -0300

    style: change styel className

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="header">
       <h1>Get Things Done</h1>
       <div className="button_container">
-        <Button text="Add new" className="add-new" />
+        <Button text="Add new" className="create" />
         <Button text="Sign Out" className="sign-out" />
       </div>
     </div>
