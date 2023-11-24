5b62d2f4f98b4e5b85e2869eed5a3cb5da1f61f0
carvalhaus <ooliveira.joaop@gmail.com>
2023-11-24 14:10:24 -0300

    feat: sort tasks by Date and map in ListItem

--- client/src/components/list/index.jsx
+++ client/src/components/list/index.jsx
@@ -1,10 +1,21 @@
 import "./style.css";
 import ListItem from "../list-item";
+import { useAPI } from "../../context/apiContext";
 
 const List = () => {
+  const { tasks } = useAPI();
+
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
+  console.log(sortedTasks);
+
   return (
     <ul className="list">
-      <ListItem task="Write an essay" />
+      {sortedTasks?.map((task) => (
+        <ListItem key={task.id} task={task.title} />
+      ))}
     </ul>
   );
 };
