b138bb0adc6c8ee17de114714c92a3b189b8a972
carvalhaus <ooliveira.joaop@gmail.com>
2023-12-12 15:23:34 -0300

    feat: add reasct-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
+import Auth from "./components/auth";
+import Signup from "./components/signup";
 import TodoApp from "./components/todoapp";
 import { APIContextProvider } from "./context/apiContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <APIContextProvider>
       <main className="main">
-        <TodoApp />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<TodoApp />} />
+            <Route path="/login" element={<Auth />} />
+            <Route path="/sign-up" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
       </main>
     </APIContextProvider>
   );
