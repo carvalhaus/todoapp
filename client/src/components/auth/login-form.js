97826b11875545d7aa6b1a9818a9c4f1a25f9aec
carvalhaus <ooliveira.joaop@gmail.com>
2024-05-26 15:20:30 -0300

    feat: create login page and validate login form

---
+++ client/src/components/auth/login-form.js
@@ -0,0 +1,108 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import axios from "axios";
+
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { useTransition } from "react";
+
+const LoginSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
+
+function LoginForm() {
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values) => {
+    startTransition(async () => {
+      try {
+        const response = await axios.post(
+          "https://jsonplaceholder.typicode.com/posts",
+          values
+        );
+
+        console.log("Data posted successfully:", response.data);
+      } catch (error) {
+        console.error("Error posting data:", error);
+      }
+    });
+  };
+
+  return (
+    <div className="w-3/4 drop-shadow-md text-lg">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="example@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="********"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button
+            type="submit"
+            className="w-full font-semibold"
+            disabled={isPending}
+          >
+            Login
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+export default LoginForm;
