9fc2681f0882cf26d0d7a943f2fde37302e28fd4
carvalhaus <ooliveira.joaop@gmail.com>
2024-05-31 19:42:02 -0300

    feat: add delete method in TableTask

--- client/src/components/app/table-task.js
+++ client/src/components/app/table-task.js
@@ -1,21 +1,30 @@
-"use client";
-
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { format } from "date-fns";
-import { FilePenLine } from "lucide-react";
+import { Check, Delete, FilePenLine } from "lucide-react";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import EditTask from "./edit-task";
+import axios from "axios";
 
-function TableTask({ data }) {
+function TableTask({ data, forceUpdate }) {
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:3001/api/tasks/${id}`)
+      .then(() => {
+        forceUpdate();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <Table>
       <TableCaption>A list of your tasks.</TableCaption>
@@ -25,7 +34,9 @@ function TableTask({ data }) {
           <TableHead>Task</TableHead>
           <TableHead className="w-44">Created at</TableHead>
           <TableHead className="w-44">Deadline</TableHead>
-          <TableHead className="text-right w-20"></TableHead>
+          <TableHead className="text-right w-5"></TableHead>
+          <TableHead className="text-right w-5"></TableHead>
+          <TableHead className="text-right w-5"></TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -35,15 +46,24 @@ function TableTask({ data }) {
             <TableCell>{task.title}</TableCell>
             <TableCell>{format(new Date(task.created_at), "PP")}</TableCell>
             <TableCell>{format(new Date(task.deadline), "PP")}</TableCell>
-            <TableCell className="text-right">
-              <Dialog>
-                <DialogTrigger>
+            <Dialog>
+              <DialogTrigger asChild>
+                <TableCell>
                   <FilePenLine color="#778599" />
-                </DialogTrigger>
-                <DialogContent>
-                  <EditTask task={task} />
-                </DialogContent>
-              </Dialog>
+                </TableCell>
+              </DialogTrigger>
+              <DialogContent>
+                <EditTask task={task} />
+              </DialogContent>
+            </Dialog>
+            <TableCell
+              className="cursor-pointer"
+              onClick={() => handleDelete(task.id)}
+            >
+              <Delete color="#778599" />
+            </TableCell>
+            <TableCell>
+              <Check color="#778599" />
             </TableCell>
           </TableRow>
         ))}
