b0ed4067d561acd7ced8efd737cc321aa796e98f
carvalhaus <ooliveira.joaop@gmail.com>
2024-05-31 19:40:12 -0300

    feat: add TaskController use

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const database = require("./services/database");
 const AuthFirebaseToken = require("./middlewares/firebase-admin");
+const TasksController = require("./controllers/TasksController");
 
 const app = express();
 
@@ -10,30 +11,10 @@ app.use(cors());
 
 const port = 3001;
 
+app.use("/api", TasksController);
+
 app.get("/api", AuthFirebaseToken, (req, res) => {
   res.send("API REQUEST RESPONSE");
-});
-
-app.get("/authors", (req, res) => {
-  database.pool
-    .query("SELECT * FROM author")
-    .then((result) => {
-      return res.status(200).json(result);
-    })
-    .catch((error) => {
-      return res.status(500).json({ error: error.message });
-    });
-});
-
-app.get("/tasks", (req, res) => {
-  database.pool
-    .query("SELECT * FROM tasks")
-    .then((result) => {
-      return res.status(200).json(result);
-    })
-    .catch((error) => {
-      return res.status(500).json({ error: error.message });
-    });
 });
 
 app.listen(port, () => {
