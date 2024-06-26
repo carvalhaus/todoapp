6e3b6f3e6388eee2e99b09cdb95a02df8ab81ad0
carvalhaus <ooliveira.joaop@gmail.com>
2024-06-26 01:02:02 -0300

    feat: create server files for deploy

--- server/src/services/database.js
+++ server/src/services/database.js
@@ -4,11 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = { pool };
