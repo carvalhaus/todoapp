e2d4846e79877986439f3a9d72926df565ebb32a
carvalhaus <ooliveira.joaop@gmail.com>
2024-06-26 19:43:54 -0300

    feat: create documentation with Swagger

--- server/src/controllers/TasksController.js
+++ server/src/controllers/TasksController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const database = require("../services/database");
+
 const {
   createTask,
   deleteTask,
@@ -8,17 +8,70 @@ const {
   selectTask,
 } = require("../models/Task");
 
+/**
+ * @swagger
+ * /tasks/{user_id}:
+ *   get:
+ *     summary: Get all tasks for a specific user
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user
+ *     responses:
+ *       '200':
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 router.get("/tasks/:user_id", async (req, res) => {
   const user_id = req.params.user_id;
 
   try {
     const result = await selectTask(user_id);
-    return res.status(201).json(result);
+    return res.status(200).json(result);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
 
+/**
+ * @swagger
+ * /tasks:
+ *   post:
+ *     summary: Create a new task
+ *     tags:
+ *       - Tasks
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       '201':
+ *         description: Task created
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 router.post("/tasks", async (req, res) => {
   const task = req.body;
 
@@ -30,6 +83,30 @@ router.post("/tasks", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   delete:
+ *     summary: Delete a task
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The task ID
+ *     responses:
+ *       '204':
+ *         description: Task deleted
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 router.delete("/tasks/:id", async (req, res) => {
   try {
     await deleteTask(req.params.id);
@@ -39,6 +116,36 @@ router.delete("/tasks/:id", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   put:
+ *     summary: Update a task
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The task ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       '204':
+ *         description: Task updated
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 router.put("/tasks/:id", async (req, res) => {
   const id = req.params.id;
   const task = req.body;
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const AuthFirebaseToken = require("./middlewares/firebase-admin");
 const TasksController = require("./controllers/TasksController");
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
@@ -10,11 +12,68 @@ app.use(cors());
 
 const port = 3001;
 
-app.use("/api", AuthFirebaseToken, TasksController);
+app.use("/api", TasksController);
 
 app.get("/api", AuthFirebaseToken, (req, res) => {
   res.send("API REQUEST RESPONSE");
 });
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Tasks API",
+    version: "1.0.0",
+    description: "A simple Express API for managing tasks",
+  },
+  components: {
+    schemas: {
+      Task: {
+        type: "object",
+        properties: {
+          id: {
+            type: "string",
+            description: "The unique identifier for the task",
+          },
+          title: {
+            type: "string",
+            description: "The title of the task",
+          },
+          created_at: {
+            type: "string",
+            format: "date-time",
+            description: "The creation date of the task",
+          },
+          duration_days: {
+            type: "integer",
+            description: "The duration of the task in days",
+          },
+          user_id: {
+            type: "string",
+            description: "The ID of the user who owns the task",
+          },
+        },
+      },
+      ErrorResponse: {
+        type: "object",
+        properties: {
+          error: {
+            type: "string",
+            description: "The error message",
+          },
+        },
+      },
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["src/controllers/*.js"], // Path to the API docs
+};
+
+const specs = swaggerjsdoc(options);
+
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
   console.log("Server is running in port:", port);
